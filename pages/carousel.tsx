1b567856783dba5afab75f8e27f2a7504da7ef16
stevendev97 <x.limsteven@gmail.com>
2022-11-06 00:05:21 -0400

    Fixed styling of grid and carousel

--- pages/carousel.tsx
+++ pages/carousel.tsx
@@ -3,16 +3,65 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import CarouselCard from "./carouselcard";
 
+
 export default function Carousel() {
     const settings = {
         infinite: false,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1
-    };
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 2200,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 1500,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+                breakpoint: 1100,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+            }
+            ,
+            {
+                breakpoint: 780,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+            }
+          ]
+        };
+    
     return (
         <div className="carousel__wrapper">
+            <h2>New Arrivals</h2>
             <Slider {...settings}>
+                <CarouselCard />
+                <CarouselCard />
+                <CarouselCard />
+                <CarouselCard />
+                <CarouselCard />
+                <CarouselCard />
+                <CarouselCard />
+                <CarouselCard />
                 <CarouselCard />
                 <CarouselCard />
                 <CarouselCard />
