b85e82ec97bc3b9f3bdcf88401eb46a1f27d6247
stevendev97 <x.limsteven@gmail.com>
2022-11-22 23:37:56 -0500

    added seller sidebar

---
+++ components/seller_sidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function SellerSidebar() {
+  return (
+    <div className='sell-sidebar-container'>
+        <div className='sell__tab'>
+            <span>All Products</span>
+        </div>
+        <div className='sell__tab'>
+            <span>Upload Product</span>
+        </div>
+    </div>
+  )
+}
+
+export default SellerSidebar
